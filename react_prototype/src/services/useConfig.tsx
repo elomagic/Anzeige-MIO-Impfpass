ac8140d2c6a22ac4cd0d9d2fd8c6c21c70bdc38b
Carsten Rambow <dev@elomagic.de>
2023-04-26 09:15:29 +0200

    Dynamic MIO bundle support added

--- react_prototype/src/services/useConfig.tsx
+++ react_prototype/src/services/useConfig.tsx
@@ -1,15 +1,20 @@
 export const initialConfig: {
-  XML_DATA: string[]
+    XML_DATA: string[]
 } = {
-  XML_DATA: [],
+    XML_DATA: [],
 }
 
 export async function loadConfig() {
-  let config = initialConfig
-  try {
-    config = await (await fetch('config.json')).json()
-  } catch (e) {
-    console.log(e)
-  }
-  return config
+
+    let dataUrl = new URLSearchParams(window.location.search).get("datasetUrl")
+    dataUrl = dataUrl ? dataUrl : 'config.json';
+
+    let config = initialConfig
+    try {
+        // TODO Authentication missing. Bearer Token from OAuth2 is preferred
+        config = await (await fetch(dataUrl)).json()
+    } catch (e) {
+        console.log(e)
+    }
+    return config
 }
